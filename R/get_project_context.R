a01aa9a098e9ad7d73841292df5cb38a5e2b1333
jcrodriguez1989 <jcrodriguez@unc.edu.ar>
2025-08-26 21:41:05 +0200

    Added machine type to the context

--- R/get_project_context.R
+++ R/get_project_context.R
@@ -3,6 +3,8 @@ get_project_context <- function() {
   list(
     r_terminal_working_directory = getwd(),
     rstudio_active_project = getActiveProject(),
-    rstudio_source_editor_context = getSourceEditorContext()$path
+    rstudio_source_editor_context = getSourceEditorContext()$path,
+    os_type = paste(.Platform$OS.type, Sys.info()[["sysname"]]),
+    architecture = Sys.info()[["machine"]]
   )
 }
