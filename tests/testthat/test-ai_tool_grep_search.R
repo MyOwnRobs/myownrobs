a858d4659b6fb3b7ece7f638c7cb2d20999f69b2
jcrodriguez1989 <jcrodriguez@unc.edu.ar>
2025-09-22 20:20:10 -0300

    Fixing unit tests for windows

--- tests/testthat/test-ai_tool_grep_search.R
+++ tests/testthat/test-ai_tool_grep_search.R
@@ -9,10 +9,7 @@ test_that("grep_search - found file", {
   )
   mock_file <- tempfile(fileext = ".R")
   writeLines("FILE_CONTENT", mock_file)
-  expect_true(grepl(
-    paste0(mock_file, "\n1:FILE_CONTENT"),
-    grep_search(list(query = "CONTENT"))$output
-  ))
+  expect_true(grepl("1:FILE_CONTENT", grep_search(list(query = "CONTENT"))$output))
 })
 
 test_that("grep_search - not found file", {
